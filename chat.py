f35a04728449b7ec47909ef4179062c3d9f769b5
Dipraj Howlader <118705873+Dipraz@users.noreply.github.com>
2024-02-10 16:53:14 +0600

    Update chat.py

--- chat.py
+++ chat.py
@@ -17,34 +17,35 @@ from IPython.display import Markdown
 
 
 def to_markdown(text):
-  text = text.replace('•', '  *')
-  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+    text = text.replace('•', ' *')
+    return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
 
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-## Function to load OpenAI model and get respones
+UX_DESIGN_PROMPT = f"""You are a friendly, kind, helpful, and highly knowledgeable world-best UX design assistant, trained on a vast dataset of UX design articles, resources, and best practices to tackle any kind of design challenge. You can ask relevant questions for better user understanding and responses, provide summaries of articles, be highly expert in generating design ideas, create prototypes, and offer feedback on UX designs. You can generate different creative text formats of text content, like codes, poems, stories, scripts, musical pieces, emails, letters, etc. You will try your best to fulfill all your and user requirements and expectations. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."""
 
+
+## Function to load OpenAI model and get responses
 def get_gemini_response(question):
     model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content(question)
+    response = model.generate_content(question, prompt=UX_DESIGN_PROMPT)  # Include the prompt here
     return response.text
 
-##initialize our streamlit app
 
-st.set_page_config(page_title="Q&A Demo")
+## Initialize our streamlit app
+st.set_page_config(page_title="UX Design Assistant")
 
-st.header("Gemini Application")
+st.header("UX Design Assistant Powered by Gemini")
 
-input=st.text_input("Input: ",key="input")
+input = st.text_input("Ask me a question about UX design: ", key="input")
 
-
-submit=st.button("Ask the question")
+submit = st.button("Get Answer")
 
 ## If ask button is clicked
-
 if submit:
-    
-    response=get_gemini_response(input)
-    st.subheader("The Response is")
+
+    response = get_gemini_response(input)
+    st.subheader("Here's my response:")
     st.write(response)
